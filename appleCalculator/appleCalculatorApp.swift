397fdf85dc1d6b1d176501a3f3970cafccc344ef
Bobby Hoopes <bobbyhoopes1@icloud.com>
2023-09-08 13:02:10 -0400

    Initial Commit

---
+++ appleCalculator/appleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleCalculatorApp.swift
+//  appleCalculator
+//
+//  Created by xcode6 on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct appleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
